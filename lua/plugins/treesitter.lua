727f09ec5d5bcd067386620a4d293d26fb477017
kelvin araque <kgiobranni@gmail.com>
2026-04-22 22:33:32 -0400

    treessiter modify

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = { "lua", "vim", "vimdoc","c","cpp" },
-      auto_install = true,
-      highlight = { enable = true },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "lua", "vim", "vimdoc", "c", "cpp" },
+			auto_install = true,
+			highlight = { enable = true, additional_vim_regex_highlighting = false },
+		},
+	},
 }
